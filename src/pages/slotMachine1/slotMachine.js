e44536026b3a1853c7f023686f2ef23945ac6d07
linj <593449387@qq.com>
2020-04-08 16:48:16 +0800

    merge

--- src/pages/slotMachine1/slotMachine.js
+++ src/pages/slotMachine1/slotMachine.js
@@ -1,6 +1,15 @@
 import Taro, { Component } from '@tarojs/taro';
 import { View, Text, Image } from '@tarojs/components';
 import './SlotMachine.less';
+const local_img_base =
+  'https://xinya-shop.oss-cn-hangzhou.aliyuncs.com/images/static/';
+const iconSlotBg = `${local_img_base}slotBg@2x.png`;
+const iconSlotBtn = `${local_img_base}slotBtn@2x.png`;
+const iconPrizeBgLeft = `${local_img_base}prizeBgLeft@2x.png`;
+const iconPrizeBgRight = `${local_img_base}prizeBgRight@2x.png`;
+const iconPrizeBg = `${local_img_base}prizeBg@2x.png`;
+const iconSlotPrizeModal = `${local_img_base}slotPrizeModal@2x.png`;
+const iconSlotPrizeHistoryModal = `${local_img_base}slotPrizeHistoryModal@2x.png`;
 
 export default class SlotMachine extends Component {
   state = {
@@ -10,29 +19,29 @@ export default class SlotMachine extends Component {
       {
         name: '一等奖',
         content: 'iphone1',
-        id: 1
+        id: 1,
       },
       {
         name: '二等奖',
         content: 'iphone11',
-        id: 2
+        id: 2,
       },
       {
         name: '三等奖',
         content: 'iphone12',
-        id: 3
-      }
+        id: 3,
+      },
     ],
     credits: 50, //积分
     curBet: 1, // 每局消耗积分
     stripHeight: 390, // 总高度
     alignmentOffset: 62, // 结果位置偏移量
     reelSpeed1Delta: 100, // 间隔位移
-    positioningTime: 200, // 停止前动画时间
+    positioningTime: 200, // 停止前动画时间 200
     bounceHeight: 85, // 结束弹射动画高度
-    firstReelStopTime: 667, // 第一个动画延迟停止时间
-    secondReelStopTime: 575, // 第二个动画延迟停止时间
-    thirdReelStopTime: 568, // 第三个动画延迟停止时间
+    firstReelStopTime: 667, // 第一个动画延迟停止时间 667
+    secondReelStopTime: 575, // 第二个动画延迟停止时间 575
+    thirdReelStopTime: 568, // 第三个动画延迟停止时间 568
     payoutStopTime: 1500, // 触发结束延迟时间
     numIconsPerReel: 3, // 每个轮子几个格子
     timer: [],
@@ -41,27 +50,140 @@ export default class SlotMachine extends Component {
       {
         top: -72, // 初始位置
         animation: '', // 结束滚动动画
-        css: '' // 反弹动画css
+        css: '', // 反弹动画css
       },
       {
         top: -202,
         animation: '',
-        css: ''
+        css: '',
       },
       {
         top: -338,
         animation: '',
-        css: ''
-      }
-    ]
+        css: '',
+      },
+    ],
+    historyRecord: [
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+      {
+        prizeName: '三等奖',
+        time: '2020-01-01 12:00:00',
+        isGet: true,
+        prizeDetailName: 'iphone10',
+      },
+    ],
+    visibleModal: false,
+    visibleHistoryModal: false,
+    isGetPrize: false,
   };
   config = {
-    navigationBarTitleText: '老虎机'
+    navigationBarTitleText: '水果机',
   };
   // runAsync 延迟返回 promise 方法
   runAsync(time) {
-    return new Promise(function(resolve, reject) {
-      const timer = setTimeout(function() {
+    return new Promise(function (resolve, reject) {
+      const timer = setTimeout(function () {
         resolve();
         clearTimeout(timer);
       }, time);
@@ -74,11 +196,11 @@ export default class SlotMachine extends Component {
       firstReelStopTime,
       secondReelStopTime,
       thirdReelStopTime,
-      payoutStopTime // 触发结束延迟时间
+      payoutStopTime, // 触发结束延迟时间
     } = this.state;
     // 点击开始后不可点击
     if (spinDisabled) return false;
-    // 随机设置奖项 该数据应该从后台接口获取
+    // 随机设置奖项 数据从后台接口获取
     let result = [];
     for (var i = 0; i < 3; i++) {
       result.push(Math.floor(Math.random() * 3 + 1));
@@ -128,7 +250,7 @@ export default class SlotMachine extends Component {
     });
     console.log('开始动画---reels---', reels);
     this.setState({
-      reels: reels
+      reels: reels,
     });
     // 循环动画
     timer[index] = setInterval(() => {
@@ -141,7 +263,7 @@ export default class SlotMachine extends Component {
       // console.log('开始动画--循环动画--reels---', reels);
       // console.log('开始动画--循环动画--timer---', timer);
       this.setState({
-        reels: reels
+        reels: reels,
       });
       if (reels[index].top > 0) {
         reels.map((item, reelsIndex) => {
@@ -152,7 +274,7 @@ export default class SlotMachine extends Component {
         });
         // console.log('开始动画--如果top大于0--reels---', reels);
         this.setState({
-          reels: reels
+          reels: reels,
         });
       }
     }, 20);
@@ -165,7 +287,7 @@ export default class SlotMachine extends Component {
       alignmentOffset, // 结果位置偏移量
       positioningTime, // 停止前动画时间
       bounceHeight, // 结束弹射动画高度
-      reels
+      reels,
     } = this.state;
     const cellHeight = stripHeight / numIconsPerReel;
     // console.log('--结束动画--cellHeight--', cellHeight);
@@ -186,7 +308,7 @@ export default class SlotMachine extends Component {
     });
     // console.log('--结束动画--reels--', reels);
     this.setState({
-      reels: reels
+      reels: reels,
     });
 
     // 到最终位置之前的动画
@@ -194,12 +316,9 @@ export default class SlotMachine extends Component {
       transformOrigin: '50% 50%',
       duration: positioningTime,
       timingFunction: 'linear',
-      delay: 0
+      delay: 0,
     });
     animation.translateY(bounceHeight).step();
-
-    // console.log('end---animation---', animation.export());
-    // console.log('end---state---', this.state);
 
     reels.map((item, reelsIndex) => {
       if (reelsIndex == index) {
@@ -208,7 +327,7 @@ export default class SlotMachine extends Component {
       return item;
     });
     this.setState({
-      reels: reels
+      reels: reels,
     });
 
     // 弹射动画
@@ -223,7 +342,7 @@ export default class SlotMachine extends Component {
         return item;
       });
       this.setState({
-        reels: reels
+        reels: reels,
       });
     });
   }
@@ -237,22 +356,25 @@ export default class SlotMachine extends Component {
       return item;
     });
     this.setState({
-      reels: reels
+      reels: reels,
     });
   }
   // 结束
   doFinish() {
     console.log('完成抽奖后的结果---', this.state.result);
-    Taro.showToast({
-      title: '恭喜你获奖了',
-      icon: 'none'
-    });
+    let isGetPrize =
+      this.state.result[0] == this.state.result[1] &&
+      this.state.result[0] == this.state.result[2] &&
+      this.state.result[1] == this.state.result[2];
+
     this.setState({
-      spinDisabled: false
+      spinDisabled: false,
+      visibleModal: true,
+      isGetPrize,
     });
   }
   render() {
-    const { prize, reels } = this.state;
+    const { prize, reels, isGetPrize, historyRecord } = this.state;
     const rule =
       '1.这里是抽奖规则说明，内容全部展示，内容文案通过后台编辑配置。\n2.这里是抽奖规则说明，内容全部展示，内容文案通过后台编辑配置。\n3.这里是抽奖规则说明，内容全部展示，内容文案通过后台编辑配置。\n4.这里是抽奖规则说明，内容全部展示，内容文案通过后台编辑配置。\n5.这里是抽奖规则说明，内容全部展示，内容文案通过后台编辑配置。\n6.这里是抽奖规则说明，内容全部展示，内容文案通过后台编辑配置。\n7.这里是抽奖规则说明，内容全部展示，内容文案通过后台编辑配置。';
     // console.log('render时候的reels', reels);
@@ -260,10 +382,7 @@ export default class SlotMachine extends Component {
     return (
       <View className="container">
         <View className="bgContainer">
-          <Image
-            src="https://xinya-shop.oss-cn-hangzhou.aliyuncs.com/images/static/slotBg@2x.png"
-            className="bgIcon"
-          ></Image>
+          <Image src={iconSlotBg} className="bgIcon"></Image>
           {/* 水果机部分 */}
           <View className="slot-machine-container">
             <View className="reel-container">
@@ -280,12 +399,17 @@ export default class SlotMachine extends Component {
             </View>
             <View className="drawNumView">
               <Text className="txt1">您共有X次摇奖机会</Text>
-              <Text className="txt2">中奖历史</Text>
+              <Text
+                className="txt2"
+                onClick={() => this.setState({ visibleHistoryModal: true })}
+              >
+                中奖历史
+              </Text>
             </View>
             {/* 点击抽奖按钮 */}
             <Image
               onClick={this.start}
-              src="https://xinya-shop.oss-cn-hangzhou.aliyuncs.com/images/static/slotBtn@2x.png"
+              src={iconSlotBtn}
               className="btnIcon"
             ></Image>
             {/* 注意事项 */}
@@ -299,15 +423,9 @@ export default class SlotMachine extends Component {
         </View>
         {/* 奖品名单-标题 */}
         <View className="prizeTitleView">
-          <Image
-            src="https://xinya-shop.oss-cn-hangzhou.aliyuncs.com/images/static/prizeBgLeft@2x.png"
-            className="prizeLeftIcon"
-          ></Image>
+          <Image src={iconPrizeBgLeft} className="prizeLeftIcon"></Image>
           <View className="prizeTxtView">
-            <Image
-              src="https://xinya-shop.oss-cn-hangzhou.aliyuncs.com/images/static/prizeBg@2x.png"
-              className="prizeIcon"
-            ></Image>
+            <Image src={iconPrizeBg} className="prizeIcon"></Image>
             <Text className="txt1">奖品名单</Text>
           </View>
         </View>
@@ -328,20 +446,101 @@ export default class SlotMachine extends Component {
         {/* 游戏规则-标题 */}
         <View className="ruleTitleView">
           <View className="ruleTxtView">
-            <Image
-              src="https://xinya-shop.oss-cn-hangzhou.aliyuncs.com/images/static/prizeBg@2x.png"
-              className="prizeIcon"
-            ></Image>
+            <Image src={iconPrizeBg} className="prizeIcon"></Image>
             <Text className="txt1">游戏规则</Text>
           </View>
-          <Image
-            src="https://xinya-shop.oss-cn-hangzhou.aliyuncs.com/images/static/prizeBgRight@2x.png"
-            className="prizeRightIcon"
-          ></Image>
+          <Image src={iconPrizeBgRight} className="prizeRightIcon"></Image>
         </View>
         {/* 游戏规则*/}
         <View className="ruleView">{Rule}</View>
         <View className="blockView"></View>
+        {/* 中奖弹窗 */}
+        {visibleModal && (
+          <View className="soltMachine-modal">
+                        <View className="soltMachine-modal-mask"></View>
+                        
+            <View className="soltMachine-modal-content">
+                          
+              <Image
+                className="soltMachine-modal-img"
+                src={iconSlotPrizeModal}
+              ></Image>
+              <Text className="txt1">
+                {isGetPrize ? '恭喜你中奖了！' : '很遗憾没有中奖哦'}
+              </Text>
+              {isGetPrize && <Text className="prizeName">iphone</Text>}
+              {!isGetPrize && (
+                <View>
+                  <Text className="noPrizeName">
+                    太可惜了竟然与奖品擦肩而过
+                  </Text>
+                  <Text className="noPrizeName1">换个姿势吧</Text>
+                </View>
+              )}
+              {isGetPrize && (
+                <View className="btnGroup">
+                  <View
+                    className="giveUpPrizeBtn"
+                    onClick={() => this.setState({ visibleModal: false })}
+                  >
+                    放弃奖品
+                  </View>
+                  <View
+                    className="getPrizeBtn"
+                    onClick={() => this.setState({ visibleModal: false })}
+                  >
+                    立即领取
+                  </View>
+                </View>
+              )}
+              {!isGetPrize && (
+                <View
+                  className="noPrizeBtn"
+                  onClick={() => this.setState({ visibleModal: false })}
+                >
+                  好的
+                </View>
+              )}
+            </View>
+                    
+          </View>
+        )}
+        {/* 中奖历史弹窗 */}
+        {visibleHistoryModal && (
+          <View className="soltMachine-history-modal">
+            <View className="soltMachine-history-modal-mask"></View>
+            <View className="soltMachine-history-modal-content">
+              <Image
+                className="iconSlotPrizeHistoryModal"
+                src={iconSlotPrizeHistoryModal}
+              ></Image>
+              <View className="soltMachine-history-modal-table">
+                {historyRecord.map((item, index) => {
+                  return (
+                    <View className="soltMachine-history-modal-item">
+                      <View className="firstView">
+                        <Text className="txt1">{item.prizeName}</Text>
+                        <Text className="txt1">{item.prizeDetailName}</Text>
+                      </View>
+                      <View className="secondView">
+                        <Text className="txt1">{item.time}</Text>
+                        <Text className="txt2">
+                          {item.isGet ? '已领取' : '未领取'}
+                        </Text>
+                      </View>
+                    </View>
+                  );
+                })}
+              </View>
+              <View
+                className="okBtn"
+                onClick={() => this.setState({ visibleHistoryModal: false })}
+              >
+                知道了
+              </View>
+            </View>
+          </View>
+        )}
       </View>
     );
   }
